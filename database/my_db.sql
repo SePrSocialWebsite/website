466211f6af4afdfd9fbf258f1101ae1542d018d6
Vytau <vytautas.skinkys@gmail.com>
2016-03-30 18:21:02 +0200

    Added files via upload

--- database/my_db.sql
+++ database/my_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2016 at 02:01 PM
+-- Generation Time: Mar 30, 2016 at 06:20 PM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 5.6.19
 
@@ -29,8 +29,24 @@ SET time_zone = "+00:00";
 CREATE TABLE `news` (
   `username` text NOT NULL,
   `news` text NOT NULL,
-  `time` date NOT NULL
+  `time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`username`, `news`, `time`) VALUES
+('aaa', 'again', '0000-00-00 00:00:00'),
+('aaa', 'aaaa', '0000-00-00 00:00:00'),
+('aaa', 'fafasdfasdf', '2016-03-30 00:00:00'),
+('aaa', 'dasdas', '2016-03-30 00:00:00'),
+('aaa', 'labas', '2016-03-30 14:29:50'),
+('aaa', 'labas kaip sekasi?', '2016-03-30 14:43:19'),
+('www', 'supp?', '2016-03-30 14:47:32'),
+('www', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', '2016-03-30 14:48:35'),
+('www', 'qwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww', '2016-03-30 14:53:09'),
+('aaa', '&lt;h1&gt;hello&lt;/h1&gt;', '2016-03-30 18:02:28');
 
 -- --------------------------------------------------------
 
@@ -42,16 +58,17 @@ CREATE TABLE `user` (
   `username` text NOT NULL,
   `password` text NOT NULL,
   `name` text NOT NULL,
-  `surrname` text NOT NULL
+  `surname` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`username`, `password`, `name`, `surrname`) VALUES
+INSERT INTO `user` (`username`, `password`, `name`, `surname`) VALUES
 ('aaa', 'aaa', '', ''),
-('qqq', 'qqq', '', '');
+('qqq', 'qqq', '', ''),
+('www', 'www', '', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
